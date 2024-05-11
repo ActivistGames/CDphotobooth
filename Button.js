916037f5e49170a3bd1e885b411f7c4d3faf6bb3
ActivistGames <74770374+ActivistGames@users.noreply.github.com>
2024-05-11 13:27:57 +0200

    Button corrections

--- Button.js
+++ Button.js
@@ -8,43 +8,51 @@ class Button {
     this.h = 50;
     this.nextStage = nextStage;
     this.easing = 0.09;
-    this.yb = height;
-    this.y2 = y;
-    this.yb2 = height;
+    this.yb = height*2;
+    this.yb2 = height*2;
+    this.hide = false;
+    this.t = 3;
   }
 
   display() {
-    let d = this.y - this.yb;
-    this.yb += d * this.easing;
-    push();
-    if (mouseX >= this.x && mouseX <= this.x + this.w && mouseY >= this.yb - this.h && mouseY <= this.yb) {
-      fill(pink);
-      if (mPressed) {
-        if (this.nextStage !== null) {
-          stage = this.nextStage;
-        } else {
-          stage++;
+    if (this.t==0) {
+      this.hide = false;
+      let d = this.y - this.yb;
+      this.yb += d * this.easing;
+      push();
+      if (mouseX >= this.x && mouseX <= this.x + this.w && mouseY >= this.yb - this.h && mouseY <= this.yb) {
+        fill(pink);
+        if (mPressed) {
+          if (this.nextStage !== null) {
+            stage = this.nextStage;
+          } else {
+            stage++;
+          }
+          this.reset();
         }
-        this.reset();
+      } else {
+        fill(darkRed);
       }
+      if (this.hide == false) {
+        noStroke();
+        rect(this.x, this.yb - this.h, this.w, this.h, 15);
+        fill(255);
+        textSize(24);
+        textAlign(CENTER, TOP);
+        let gr = 0;
+        if (currentLayout == "greek") {
+          gr = 5;
+        }
+        text(getTranslation(currentLayout, this.txt), this.x + this.w / 2, this.yb - this.h + this.h / 4 - gr);
+      }
+      pop();
     } else {
-      fill(darkRed);
+      this.t--;
     }
-
-    noStroke();
-    rect(this.x, this.yb - this.h, this.w, this.h, 15);
-    fill(255);
-    textSize(24);
-    textAlign(CENTER, TOP);
-    let gr = 0;
-    if (currentLayout == "greek") {
-      gr = 5;
-    }
-    text(getTranslation(currentLayout, this.txt), this.x + this.w / 2, this.yb - this.h + this.h / 4 - gr);
-    pop();
   }
   reset() {
-    this.y = this.y2;
     this.yb = this.yb2;
+    this.t = 4;
+    this.hide = true;
   }
 }
