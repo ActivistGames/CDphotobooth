986392f300f6c950730b80a29b0a1ff479806be8
ActivistGames <74770374+ActivistGames@users.noreply.github.com>
2024-05-14 12:55:30 +0200

    const update
    dutch correction

--- const.js
+++ const.js
@@ -90,7 +90,7 @@ const translations = {
   },
   dutch: {
     "Make your own poster": "Maak je eigen poster",
-    "Express your feelings about Democracy": "Uit je gevoelens over democratie",
+    "Express your feelings about Democracy": "Voeg jouw inzicht over democratie toe",
     "What is your name?": "Hoe heet je?",
     "Write your statement": "Schrijf je verklaring",
     "Next": "Volgende",
